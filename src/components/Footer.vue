ea76ff1742e47da86f6902a3861434c9a36d2204
koval2001 <dianakoval2001@icloud.com>
2020-09-26 23:07:13 +0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-    <h2>I am footer</h2>
+    <div id="footer">
+        <div class="container">
+            <div class="row justify-content-center">
+                <p class="pFooter">All rights reserved.<br>Copyrights to <span class="gold">WebFlow</span>.</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
