1df978fed7a71228a6b9d8a4ebf1d088573ae2ad
anastasiatarannn <anastasiatarannn@icloud.com>
2020-10-01 15:08:31 +0300

    taranl

---
+++ src/views/Categories.vue
@@ -0,0 +1,106 @@
+<template>
+    <div id="categories">
+        <div class="container">
+            <div class="cat-wrap row">
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat1.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Комедіі</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat2.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Мелодрами</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat3.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Детективи</a>
+                    </div>
+                </div>
+            </div>
+            <div class="cat-wrap row">
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat4.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Боєвики</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat5.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Фентезі</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat6.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Драми</a>
+                    </div>
+                </div>
+            </div>
+            <div class="cat-wrap row">
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat7.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Сімейні</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat8.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Документальні</a>
+                    </div>
+                </div>
+                <div class="cat-wrap--card col-4">
+                    <div class="cat-wrap--card---img">
+                        <img src="../../public/img/categories/cat9.png" alt="">
+                    </div>
+                    <div class="cat-wrap--cars---text">
+                        <a href="#">Жахи</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="categories-mob">
+                <div class="categories-mob--bac">
+                      <h3>Жанри</h3>
+                        <div class="categories-mob--wrap">
+                         <a href="#">Комедіі</a>
+                         <a href="#">Мелодрами</a>
+                         <a href="#">Детективи</a>
+                         <a href="#">Драми</a>
+                         <a href="#">Боєвики</a>
+                         <a href="#">Фєнтезі</a>
+                         <a href="#">Cімейні</a>
+                         <a href="#">Документальні</a>
+                         <a href="#">Жахи</a>
+                        </div>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Categories'
+    }
+</script>
