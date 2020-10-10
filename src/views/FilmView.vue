014970ea6df5fc78abfcf9ae18eeaef4aef49571
koval2001 <dianakoval2001@icloud.com>
2020-10-10 13:06:04 +0300

    filmpage markup

--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,18 +1,85 @@
 <template>
     <div id="film-view">
         <div class="container container-fluid">
-            <div class="row">
+            <div class="row justify-content-between">
                 <div class="col-7">
                     <div class="film-description">
-                        <h3 class="hFilmText"></h3>
+                        <h3 class="hText">Wonder Park</h3>
                         <div class="fBox">
                             <p>21 марта, 2019</p>
                             <p>YouFilm: 5,8\10</p>
                             <p>США</p>
                         </div>
+                        <div class="fText">
+                            <p>Pellentesque quis dui varius, dapibus velit
+                                id, iaculis ipsum. Morbi ac eros feugiat,
+                                lacinia elit ut elementum turpis. Curabitur justo
+                                sapien, tempus sit amet rutrum eu, commodo eu lacus.
+                                Morbi in ligula nibh. Maecenas ut mi at odio hendrerit
+                                eleifend tempor vitae augue. Fusce eget arcu et
+                                nibh dapibus maximus consectetur in est.
+                            </p>
+                        </div>
+                        <div class="fTable">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td>Країна</td>
+                                        <td>США</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Актори</td>
+                                        <td>Sofia Mali, Jennifer Garner, Ken Hudson Campbell</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Тривалість</td>
+                                        <td>1h 29m</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Жанр</td>
+                                        <td>Animation, Adventure, Comedy</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Режисер</td>
+                                        <td>Animation, Adventure, Comedy</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Нагороди</td>
+                                        <td>Animation, Adventure, Comedy</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <div class="col-5"></div>
+                <div class="col-4">
+                    <div class="imgWrap">
+                        <img class="img-fluid" src="../../public/img/films/fim1.jpeg" alt="film_poster">
+                    </div>
+                    <div class="imgStars">
+                        <div class="rating">
+                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                        </div>
+                        <p>рейтинг: 7/10</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="hText">Відгуки</h3>
+                    <div class="comment crComment">
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="usWrap">
+                                    <img class="img-fluid" src="../../public/img/icon_user.svg" alt="user icon">
+                                    <p>Головна вовчиця</p>
+                                </div>
+                            </div>
+                            <div class="col-10"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
