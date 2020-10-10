0f29cdac21dbb55a8a995e14de6d0fba28db1431
koval2001 <dianakoval2001@icloud.com>
2020-10-10 16:36:59 +0300

    film page desktop

--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -68,7 +68,7 @@
             <div class="row">
                 <div class="col-12">
                     <h3 class="hText">Відгуки</h3>
-                    <div class="comment crComment">
+                    <div class="comment crComment container">
                         <div class="row">
                             <div class="col-2">
                                 <div class="usWrap">
@@ -76,9 +76,43 @@
                                     <p>Головна вовчиця</p>
                                 </div>
                             </div>
-                            <div class="col-10"></div>
+                            <div class="col-10">
+                                <form id="cForm">
+                                    <textarea id="cArea" rows="3" placeholder="Залишити відгук"></textarea>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-left">
+                                <button class="btn btn-comment">Надіслати</button>
+                            </div>
                         </div>
                     </div>
+                    <div class="comment rComment container">
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="usWrap">
+                                    <img class="img-fluid" src="../../public/img/icon_user.svg" alt="user icon">
+                                    <p>Головна вовчиця</p>
+                                </div>
+                            </div>
+                            <div class="col-10">
+                                <div class="cTextWrap">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Donec posuere metus et tortor pulvinar venenatis.
+                                        Aliquam erat volutpat. Nam ultrices semper felis, at laoreet metus
+                                    </p>
+                                    <p>
+                                        оцінка: <span class="points">7/10</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    <button class="btn bGold">Більше</button>
                 </div>
             </div>
         </div>
