a9607f305625e7378fe79d114c32c8ef44060b72
koval2001 <dianakoval2001@icloud.com>
2020-10-12 18:31:36 +0300

    fix film page

--- src/views/Films.vue
+++ src/views/Films.vue
@@ -1,16 +1,26 @@
 <template>
-    <div id="films">
-        <div class="films-wrap container">
-            <div class="films-text">
-                <h1>Фільми</h1>
-                <p>Миттєвий пошук фільмів та зручний серфінг по сайту.<br>Ви завжди будете в курсі коли і що дивитись.</p>
+    <div>
+        <div id="films">
+            <div class="films-wrap container">
+                <div class="films-text">
+                    <h1>Фільми</h1>
+                    <p>Миттєвий пошук фільмів та зручний серфінг по сайту.<br>Ви завжди будете в курсі коли і що дивитись.</p>
+                </div>
             </div>
         </div>
+        <Categories />
     </div>
 </template>
 
 <script>
-export default {
-    name: 'Films'
-}
+    // @ is an alias to /src
+    import Categories from "./Categories";
+
+
+    export default {
+        name: 'Films',
+        components: {
+            Categories
+        }
+    }
 </script>
