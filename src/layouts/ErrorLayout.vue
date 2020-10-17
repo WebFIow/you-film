5aa4f04972c99f0daa96327f46a1333e71859476
koval2001 <dianakoval2001@icloud.com>
2020-10-17 18:44:30 +0300

    error page

--- src/layouts/ErrorLayout.vue
+++ src/layouts/ErrorLayout.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1>Oops, 404</h1>
+    <div id="error">
+        <div class="error-wrap">
+        <div class="container">
+            <div class="error-text">
+                <h1>Ой, дитино, ну і де ти лазиш?</h1>
+                <h1> Чув дзвін, та не знаю звідки він.</h1>
+            </div>
+        </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
