641351bbf0ab0ca4e8e23228511f20a65c166b0b
koval2001 <dianakoval2001@icloud.com>
2020-10-23 22:56:31 +0300

    user data in profile

--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -1,32 +1,53 @@
 <template>
   <div class="tab-content">
-    <p>
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-      nulla?
-    </p>
+    <form>
+      <div class="form-group">
+        <label for="name_login">Ім’я в YouFilm</label>
+        <div class="d-flex justify-content-between">
+          <input type="text" class="form-control" id="name_login" placeholder="Одри Хепбёрн" disabled>
+          <button type="submit" class="btn btn-mdl">Змінити</button>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <div class="d-flex justify-content-between">
+          <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="moviesaremylife@you-film.ua" disabled>
+          <button type="submit" class="btn btn-mdl">Змінити</button>
+        </div>
+      </div>
+
+      <button class="accordion" @click="showAcc">Змінити пароль</button>
+      <div class="panel">
+        <ul class="accUL">
+          <li>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-    data:() => ({
+    data: () => ({
 
     }),
     methods: {
-      name() {
-        
-      }
-    },    
-  }
+      showAcc(e) {
+        const el = e.target;
+        el.classList.toggle("active");
+        const panel = el.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 70 + "px";
+        }
+      },
+    },
+  };
 </script>
