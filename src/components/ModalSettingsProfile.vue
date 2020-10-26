5ea75898b4629443ec9dda37d43cda724256e708
koval2001 <dianakoval2001@icloud.com>
2020-10-26 20:22:55 +0200

    modal for profile

---
+++ src/components/ModalSettingsProfile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div 
+        id="mdlAddToWatch"
+        @click="close" 
+        ref="modalWrapper"
+    >
+
+        <div class="mdl">
+            <div class="mdl-content">
+                <p>Для підтвердження зміни пошти, ми надіслали лист на mynewmegaemail@gmail.com. Слідуйте інструкції.</p>
+                <button type="submit" class="btn btn-mdl">Добре</button>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+    data: () => ({
+
+    }),
+    methods: {
+        close(e) {
+            if (this.$refs.modalWrapper == e.target || this.$refs.modalClose == e.target) {
+                this.$emit('close')
+            }
+        }
+    }
+}
+</script>
